52c43f9e0586da11f61a90bbf1ad095ba4a73aae
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2008-11-18 18:12:03 +0100

    Fix error field name

--- templates/dj10n/wip-translator.php
+++ templates/dj10n/wip-translator.php
@@ -81,8 +81,8 @@ $mail="{{ mail|safe }}";
         <td class='bookingDate'>{{ po.startdate|date:"j b y"}}</td>
         <td>{{ po.untranslated }}</td>
         <td>{{ po.fuzzy }}</td>
-        <td class="pology"><a href='../pology-errors.php?po={{ po.name }}.po&amp;package={{ module.name }}&amp;branch={{ branch.path }}&amp;mode=gui'>{{ po.errortho }}
-        {% ifequal po.errortho 0 %}<img src='../img/status/ok.png' alt='Aucune erreur' />
+        <td class="pology"><a href='../pology-errors.php?po={{ po.name }}.po&amp;package={{ module.name }}&amp;branch={{ branch.path }}&amp;mode=gui'>{{ po.error }}
+        {% ifequal po.error 0 %}<img src='../img/status/ok.png' alt='Aucune erreur' />
         {% else %}<img src='../img/status/warning.png' alt='Erreurs Pology' /> {% endifequal %}</a></td>
         <td>{{ po.translated }}</td>
         </tr>

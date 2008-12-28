c9d5fc2bbb9144d500363ab79ed7b2a516645587
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2008-12-28 21:50:33 +0100

    Check moved file with pology

--- dj10n/commit.py
+++ dj10n/commit.py
@@ -6,14 +6,19 @@
 """Tools to ease coordinator workflow"""
 
 from py10n.dj10n.models import Pofile
+from py10n.dj10n.pology import posieve
+from py10n.settings import PY10N_LANG
 
 from os import listdir
 from os.path import join
 from shutil import move
 
-def movePos(path, type="gui"):
+def movePos(path, type="gui", check=True):
     """Move po file to their good place in SVN prior to commit
-    @param pos: list of po file names"""
+    @param pos: list of po file names
+    @param type: gui or doc
+    @type type: str
+    @param check: also check files with Pology (default is True)"""
     pos=[f for f in listdir(path) if f.endswith(".po")]
     if len(pos)==0:
         print "No po file found"
@@ -21,10 +26,17 @@ def movePos(path, type="gui"):
     print "About to move %s pos" % len(pos)
     for poName in pos:
         try:
+            print "======== %s ========" % poName
             po=Pofile.objects.get(name=poName[:-3], type=type)
             destPath=po.poFilePath()
             move(join(path, poName), destPath)
             print "Move %s to %s (booked by %s)" % (poName, destPath, po.translator)
+            if check:
+                for sieve, options in (("check_rules", (("lang", PY10N_LANG),)),
+                                       ("check_spell", (("lang", PY10N_LANG),)),
+                                       ("-c check_xml-kde4", ())):
+                    print "Checking %s with %s" % (poName, sieve)
+                    posieve(sieve, options, destPath)
         except Pofile.DoesNotExist:
             print "%s not found. Skipping" % poName
             continue

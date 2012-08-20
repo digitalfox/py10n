59c6f3a084b0f05cb8a22f35968f735b7dadbf5a
Sébastien Renard <Sebastien.Renard@digitalfox.org>
2012-08-20 13:51:14 +0200

    Don't consider module with no pofile. Some PEP8 coding style fixes

--- py10nMain.py
+++ py10nMain.py
@@ -27,102 +27,102 @@ from sys import exit
 import codecs
 import re
 
-SIEVES={"check_rules":"pology-rules-errors.xml",
+SIEVES = {"check_rules":"pology-rules-errors.xml",
        "check-spell":"pology-spell-errors.xml"}
 
 
 def poStat(pologyXmlStat, type='gui'):
     """Update PO statistics"""
     for po in Pofile.objects.filter(type=type):
-        path=po.poFilePath()
+        path = po.poFilePath()
         if not exists(path):
-            path=po.filePath()
+            path = po.filePath()
 
-        md5sum=computePoHashValue(path)
-        if po.md5sum!=md5sum:
+        md5sum = computePoHashValue(path)
+        if po.md5sum != md5sum:
             print "Processing %s" % po.name
             updateGettextStats(po)
             updatePologyStats(po, pologyXmlStat)
-            po.md5sum=md5sum
+            po.md5sum = md5sum
             po.save()
 
 def readPologyXmlStat(pologyXmlPath):
     """Read number of errors for each po in pology xml error summary
     @return: hash array with po name as key and number of error as value"""
-    pologyXmlStat={}
-    poString=re.compile(u'<po name="(.+).po">')
+    pologyXmlStat = {}
+    poString = re.compile(u'<po name="(.+).po">')
     print "Load errors from pology xml file..."
     for xmlFile in SIEVES.values():
         for line in codecs.open(join(pologyXmlPath, xmlFile), "r", "utf-8"):
-            result=poString.match(line)
+            result = poString.match(line)
             if result:
-                count=0
-                name=result.group(1)
+                count = 0
+                name = result.group(1)
             elif u"<error>" in line:
-                count+=1
+                count += 1
             elif u"</po>" in line:
                 if pologyXmlStat.has_key(name):
-                    pologyXmlStat[name]+=count
+                    pologyXmlStat[name] += count
                 else:
-                    pologyXmlStat[name]=count
+                    pologyXmlStat[name] = count
     return pologyXmlStat
 
 def createPologyXMLStat(pologyXmlPath, type="gui", sieve="check_rules"):
     """Create POlogy xml errors file for all modules
     @param pologyXMLPath: Path where XML file will be write"""
     #TODO: use new function from Pology modules
-    mPath=" ".join([join(settings.PY10N_FILE_BASEPATH, m.poPath()) for m in Module.objects.filter(type=type)])
-    posieve(sieve, [("xml", join(pologyXmlPath, SIEVES[sieve])), ("lang", settings.PY10N_LANG)], mPath) 
+    mPath = " ".join([join(settings.PY10N_FILE_BASEPATH, m.poPath()) for m in Module.objects.filter(type=type) if m.pofile_set.count() > 0])
+    posieve(sieve, [("xml", join(pologyXmlPath, SIEVES[sieve])), ("lang", settings.PY10N_LANG)], mPath)
 
 def sync(type="gui"):
     """Sync filesystem modules and po files with database"""
 
-    if type=="gui":
-        modulePath="templates/messages"
+    if type == "gui":
+        modulePath = "templates/messages"
     else:
-        modulePath="templates/docmessages"
+        modulePath = "templates/docmessages"
 
-    dbTrunkModuleList=[m.name for m in Module.objects.filter(type=type)]
+    dbTrunkModuleList = [m.name for m in Module.objects.filter(type=type)]
 
     for branch in Branch.objects.all():
-        path=join(settings.PY10N_FILE_BASEPATH, branch.path, modulePath)
+        path = join(settings.PY10N_FILE_BASEPATH, branch.path, modulePath)
         try:
-            fsModuleList=listdir(path)
+            fsModuleList = listdir(path)
         except OSError, e:
             print "Cannot find template dir for branch %s (%s)" % (branch.name, e)
             continue
-        
-        if branch.name=="trunk":
+
+        if branch.name == "trunk":
             # Adding modules in trunk only
             for moduleName in fsModuleList:
-                if moduleName==".svn" or moduleName in dbTrunkModuleList:
+                if moduleName == ".svn" or moduleName in dbTrunkModuleList:
                     continue
                 else:
                     print "Adding %s (%s) module to database" % (moduleName, branch.name)
-                    m=Module()
-                    m.branch=branch
-                    m.name=moduleName
-                    m.type=type
+                    m = Module()
+                    m.branch = branch
+                    m.name = moduleName
+                    m.type = type
                     m.save()
-        
+
         # Removing modules
         for module in branch.module_set.filter(type=type):
             if module.name not in fsModuleList:
-                if module.pofile_set.count()==0:
+                if module.pofile_set.count() == 0:
                     print "Removing module %s (%s)" % (module.name, module.branch.name)
                     module.delete()
                 else:
                     print "Module %s (%s) does not exist anymore but still have pofile." % (module.name, branch.name)
 
     # Adding/removing po
-    poToBeRemoved=set() # Don't destroyed immediately PO to detect their move
-    
+    poToBeRemoved = set() # Don't destroyed immediately PO to detect their move
+
     # Removing po
     for module in Module.objects.filter(type=type):
-        path=join(settings.PY10N_FILE_BASEPATH, module.branch.path, modulePath, module.name)
-        dbPoList=[f.name for f in module.pofile_set.all()]
+        path = join(settings.PY10N_FILE_BASEPATH, module.branch.path, modulePath, module.name)
+        dbPoList = [f.name for f in module.pofile_set.all()]
         try:
-            fsPoList=[p[:-4] for p in listdir(path) if p!=".svn"]
+            fsPoList = [p[:-4] for p in listdir(path) if p != ".svn"]
             #print list(fsPoList)
         except OSError, e:
             print "Cannot find module %s at %s (%s). Skipping" % (module.name, path, e)
@@ -135,10 +135,10 @@ def sync(type="gui"):
 
     # Adding po (in a separate loop to track on po move)
     for module in Module.objects.filter(type=type):
-        path=join(settings.PY10N_FILE_BASEPATH, module.branch.path, modulePath, module.name)
-        dbPoList=[f.name for f in module.pofile_set.all()]
+        path = join(settings.PY10N_FILE_BASEPATH, module.branch.path, modulePath, module.name)
+        dbPoList = [f.name for f in module.pofile_set.all()]
         try:
-            fsPoList=[p[:-4] for p in listdir(path) if p[-4:]==".pot"]
+            fsPoList = [p[:-4] for p in listdir(path) if p[-4:] == ".pot"]
             #print list(fsPoList)
         except OSError, e:
             print "Cannot find module %s at %s (%s). Skipping" % (module.name, path, e)
@@ -150,18 +150,18 @@ def sync(type="gui"):
             else:
                 # Looking for po to be removed that can be undeleted
                 for po in poToBeRemoved:
-                    if po.name==poName and po.type==type:
+                    if po.name == poName and po.type == type:
                         print "Moving %s from %s to %s" % (po.name, po.module.name, module.name)
-                        po.module=module
+                        po.module = module
                         po.save()
                         poToBeRemoved.remove(po)
                         break
                 else:
                     print "Adding %s in %s module" % (poName, module.name)
-                    p=Pofile()
-                    p.module=module
-                    p.name=poName
-                    p.type=type
+                    p = Pofile()
+                    p.module = module
+                    p.name = poName
+                    p.type = type
                     p.save()
     # Really remove po that no longer exist
     for po in poToBeRemoved:
@@ -170,8 +170,8 @@ def sync(type="gui"):
 
 def parseOptions():
     """Command line option parsing"""
-    parser=OptionParser()
-    
+    parser = OptionParser()
+
     # Pages generation
     parser.add_option("-f", "--file", dest="filename",
               help="Write page to FILE")
@@ -205,11 +205,11 @@ def parseOptions():
     parser.add_option("-m", "--move", dest="move",
               help="Move po files to the right place to be committed")
     return parser.parse_args()
-    
+
 def main():
     """Main entry point"""
-    (options, args)=parseOptions()
-    
+    (options, args) = parseOptions()
+
     # What do we want to do ?
     if options.shell:
         Shell().loop()
@@ -223,9 +223,9 @@ def main():
         exit(1)
 
     if options.doc:
-        type="doc"
+        type = "doc"
     elif options.gui:
-        type="gui"
+        type = "gui"
     else:
         print "You must choose either doc (-d) or gui (-g)"
         exit(1)
@@ -241,14 +241,14 @@ def main():
         if not checkFile(options.filename):
             exit(1)
         # Choose correct type
-        if options.type=="po":
+        if options.type == "po":
             print "Creating PO page"
             file(options.filename, "w").write(bookingPage(type).encode("UTF-8"))
-        elif options.type=="translator":
+        elif options.type == "translator":
             print "Creating Translator page"
             file(options.filename, "w").write(translatorsPage(type).encode("UTF-8"))
-            file(options.filename[:-3]+"csv", "w").write(translatorsPage(type, format="csv").encode("UTF-8"))
-        elif options.type=="stat":
+            file(options.filename[:-3] + "csv", "w").write(translatorsPage(type, format="csv").encode("UTF-8"))
+        elif options.type == "stat":
             print "Creating statistics page"
             file(options.filename, "w").write(statsPage(type).encode("UTF-8"))
         else:
@@ -272,7 +272,7 @@ def main():
             print "pology XML path must be defined for this action"
             exit(1)
     elif options.move:
-        path=abspath(options.move)
+        path = abspath(options.move)
         if not isdir(path):
             print "%s is not a directory" % options.move
             exit(1)
@@ -280,6 +280,6 @@ def main():
     else:
         print "Use --help for help"
         exit(1)
-      
+
 if __name__ == "__main__":
     main()
